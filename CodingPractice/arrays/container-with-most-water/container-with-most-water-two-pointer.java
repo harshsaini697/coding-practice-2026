877bb4e32302c165ca9c9862f4d771b428cc2364
harsh <harshsaini697@gmail.com>
2026-05-08 17:38:47 -0500

    description
    Signed-off-by: harsh <harshsaini697@gmail.com>

--- CodingPractice/arrays/container-with-most-water/container-with-most-water-two-pointer.java
+++ CodingPractice/arrays/container-with-most-water/container-with-most-water-two-pointer.java
@@ -23,3 +23,9 @@ class Solution {
         return max;
     }
 }
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+// The brute-force approach would be to check all possible pairs of lines and calculate the area for each pair, which would have a time complexity of O(n^2). 
+// The two-pointer approach optimizes this by starting with the widest possible container and then moving the pointers inward based on the heights of the lines, resulting in a linear time complexity of O(n).
